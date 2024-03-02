91d597afdceecec69df4699718618e1e06291b39
NadavETheFirst <nadav.elsberg1@gmail.com>
2024-03-02 22:05:25 +0200

    first commit. created function that gets an 5x5 array filled in random

---
+++ PhotoCreator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PhotoCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] pixels = CreatePixelArray();
+            Console.WriteLine("here it is:");
+            printArray(pixels);
+            
+        }
+
+        public static int[,] CreatePixelArray(){
+            int[,] pixels = new int[5,5];
+            for(int i =0; i<5;i++)
+            {
+                for(int j =0; j<5;j++)
+                {
+                    int value = new Random().Next(0,8);
+                    pixels[i,j] = value;
+                }                
+            }
+            return pixels;
+        }
+        public static void printArray(int[,] array){
+            for(int i=0; i<array.GetLength(0);i++){
+                for(int j=0; j<array.GetLength(1);j++){
+                    Console.Write(array[i,j]+", ");                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
